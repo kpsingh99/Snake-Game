ece0c80a209a70516b8aed41d48e478e99cfa65e
sigmadonaldduck <kpsingh56749@gmail.com>
2024-06-03 11:40:50 +0530

    used test_surface to get rect object and then used it to manipulate the position of test_surface

--- snake.py
+++ snake.py
@@ -9,8 +9,9 @@ clock = pygame.time.Clock()
 test_surface = pygame.Surface((100, 200))
 test_surface.fill((0, 0, 255))
 
-# Rect allows us to control more points rather than just the top left point
-test_rect = pygame.Rect(100, 200, 100, 101)  # Rect(x, y, w, h) where (x,y) is coordinates of top left point
+# test_rect = test_surface.get_rect(center=(200, 250))
+test_rect = test_surface.get_rect(topright=(200, 250))
+
 
 while True:
     for event in pygame.event.get():
@@ -20,11 +21,9 @@ while True:
 
     # screen.fill(pygame.Color('gold'))
     screen.fill((175, 215, 70))
-    pygame.draw.rect(screen, pygame.Color('Red'), test_rect)  # pygame.draw.rect(surface, color, rect)
-    # we could also use it to draw other shapes like pygame.draw.ellipse() etc
 
-    # specifies where the top left corner of our test_surface will be placed
-    screen.blit(test_surface, (200, 250))  # block image transformer
+    test_rect.left += 1
+    screen.blit(test_surface, test_rect)
 
     pygame.display.update()
     clock.tick(60)
