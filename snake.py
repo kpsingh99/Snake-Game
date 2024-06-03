efe2070149abb5cda0cab1016ef07fe0d3bbce64
sigmadonaldduck <kpsingh56749@gmail.com>
2024-06-03 11:23:05 +0530

    added clock.tick  animated test_surface

--- snake.py
+++ snake.py
@@ -9,6 +9,8 @@ clock = pygame.time.Clock()
 test_surface = pygame.Surface((100, 200))
 test_surface.fill((0, 0, 255))
 
+x_pos = 200
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -17,11 +19,13 @@ while True:
 
     # screen.fill(pygame.Color('gold'))
     screen.fill((175, 215, 70))
+    x_pos += 1
 
     # specifies where the top left corner of our test_surface will be placed
-    screen.blit(test_surface, (200, 250))  # block image transformer
-
+    screen.blit(test_surface, (x_pos, 250))  # block image transformer
 
     pygame.display.update()
+    clock.tick(60)
+
 
 
